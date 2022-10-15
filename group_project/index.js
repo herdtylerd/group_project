62a237dea396b1eb3cdbca8d43bcf3dd22bff31f
herdtylerd <91562573+herdtylerd@users.noreply.github.com>
2022-10-14 21:15:17 -0600

    first commit

---
+++ group_project/index.js
@@ -0,0 +1,28 @@
+const {Restaurant} = require("./myrestaurants");
+
+const express = require("express");
+
+//const ejsLint = require('ejs-lint');
+
+let app = express();
+
+let path = require("path");
+
+const port = 3000;
+
+app.use(express.urlencoded({extended : true}));
+
+app.use(express.static(__dirname + '/public/'));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    let restaurants = new Array();
+
+    restaurants.push(new Restaurant("American", "J-Dawgs"));
+    restaurants.push(new Restaurant("American", "JCW's"));
+
+    res.render("index", {restaurants : restaurants});
+});
+
+app.listen(port, () => console.log("Website has started!"));
---
+++ group_project/myrestaurants.js
@@ -0,0 +1,10 @@
+class Restaurant
+{
+    constructor(rest_type, rest_name)
+    {
+        this.rest_name = rest_name;
+        this.rest_type = rest_type;
+    }
+}
+
+module.exports.Restaurant = Restaurant;
