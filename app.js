d4ee9a8f513c56bba0540a8b45cf374aa01ff481
herdtylerd <91562573+herdtylerd@users.noreply.github.com>
2022-10-15 00:37:31 -0600

    db stuff

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const express = require("express");
 
 let app = express();
 
+const knex = require("knex")({
+    client:"mysql",
+    connection: {
+    host:"restaurantdb.cluster-cjnzdf71h3hb.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "admin1234",
+    database:"provoFood",
+    port: 3306,
+    },
+    });
+
+
+
 let path = require("path");
 
 const port = 3001;
@@ -14,10 +27,14 @@ app.use(express.static(__dirname + '/public/'));
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-
-
-    res.render("index");
+app.get("/",(req,res) => {
+    knex
+    .select()
+    .from("restaurants")
+    .then((result) => {
+    console.log(result);
+    res.render("index", {restaurants : result});
+    });
 });
 
 app.listen(port, () => console.log("Website has started!"));
